609d5d56c1fda1c53fd61a323dd2032f29d062ec
zhangzhuo <862340288@qq.com>
2019-04-08 13:21:50 +0800

    init

---
+++ mijia.ts
@@ -0,0 +1,12 @@
+//% weight=0 color=55 icon="\uf13d" block="MiJia"
+namespace mijia { 
+
+
+    //% blockId="hello" block=" compute number x %x"
+    //% blockGap=2 weight=0 blockExternalInputs = true
+    export function hello(x: number): number { 
+        return 2*x;
+    }
+
+}
+

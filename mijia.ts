4f47974a04885aaf081cd33b6dce1eeda83961e3
zhangzhuo <862340288@qq.com>
2019-04-08 19:33:59 +0800

    1

--- mijia.ts
+++ mijia.ts
@@ -1,8 +1,8 @@
 //% weight=0 color=55 icon="\uf13d" block="mijia"
 namespace mijia { 
 
-    //% blockId="hello" block="compute number x %x"
-    //% blockGap=2 weight=0 blockExternalInputs=true
+    //% blockId=hello block="compute number x %x"
+    //% blockGap=2 weight=1 blockExternalInputs=true
     export function hello(x: number): number { 
         return 2*x;
     }
